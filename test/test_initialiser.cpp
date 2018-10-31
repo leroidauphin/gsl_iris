bb32550d121172bd225c3e0f79f49efebf5f05c1
leroidauphin <johnangusmcleod@gmail.com>
2018-10-31 22:12:08 +0000

    Fix test

--- test/test_initialiser.cpp
+++ test/test_initialiser.cpp
@@ -8,9 +8,5 @@ TEST(sample_initialiser, test_gaussian_initialiser) {
 	auto initialiser = GaussianInitialiser::make_initialiser();
 	auto value = initialiser->get_value();
 
-	std::cout << value << std::endl;
-
-	EXPECT_DOUBLE_EQ(value, 0.133919);  // segfaults?
-
-	std::cout << value << std::endl;
+	EXPECT_DOUBLE_EQ(value, 0.1339186081186759);
 }
