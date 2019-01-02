06adb7c06a099b70b341c289bed57db1a16526c3
leroidauphin <johnangusmcleod@gmail.com>
2019-01-02 13:45:34 +0000

    Fill out dummy model test points

--- test/test_model.cpp
+++ test/test_model.cpp
@@ -17,6 +17,10 @@ class Variables<int> {
 			return values;
 		}
 
+		void set_values(int value) {
+			values = value;
+		}
+
 		Variables(int values): values(values)
 		{}
 
@@ -25,6 +29,21 @@ class Variables<int> {
 		int values;
 };
 
+
+class IntLayer: public Layer<int> {
+	public:
+		~IntLayer() = default;
+
+		void apply(std::shared_ptr<Variables<int>> input) {
+			input->set_values(input->get_values() * layer);
+		}
+
+		IntLayer(int layer): layer(layer)
+		{}
+	private:
+		int layer;
+
+};
 
 
 TEST(test_model, test_no_layers){
@@ -42,8 +61,31 @@ TEST(test_model, test_no_layers){
 
 
 TEST(test_model, test_one_layer){
+	auto one_layer = std::make_shared<IntLayer>(3);
+	std::list<std::shared_ptr<Layer<int>>> list{one_layer};
+	Model<int> model(list);
+
+	auto variables_initialiser = 2;
+	auto variable_ptr = std::make_shared<Variables<int>>(variables_initialiser);
+
+	model.apply(variable_ptr);
+	auto output_values = variable_ptr->get_values();
+
+	ASSERT_EQ(output_values, 6.0);
 }
 
 
 TEST(test_mode, test_two_layers){
+	auto one_layer = std::make_shared<IntLayer>(3);
+	auto two_layer = std::make_shared<IntLayer>(5);
+	std::list<std::shared_ptr<Layer<int>>> list{one_layer, two_layer};
+	Model<int> model(list);
+
+	auto variables_initialiser = 2;
+	auto variable_ptr = std::make_shared<Variables<int>>(variables_initialiser);
+
+	model.apply(variable_ptr);
+	auto output_values = variable_ptr->get_values();
+
+	ASSERT_EQ(output_values, 30.0);
 }
