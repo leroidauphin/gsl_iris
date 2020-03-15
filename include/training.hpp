d109b0eff3e4053b25e1cbdc1652c7697b86693d
leroidauphin <johnangusmcleod@gmail.com>
2020-03-15 22:30:24 +0000

    Start to add batch cost function

--- include/training.hpp
+++ include/training.hpp
@@ -16,4 +16,38 @@ class Cost {
 };
 
 
+template<class T>
+class BatchCost {
+    public:
+        virtual double compute_cost(
+            std::list<std::shared_ptr<Variables<T>>> predictions,
+            std::list<std::shared_ptr<Variables<T>>> targets
+	) = 0;
+};
+
+
+template<class T>
+class MeanBatchCost : public BatchCost<T> {
+    public:
+	MeanBatchCost(Cost<T> single_cost): single_cost(single_cost)
+	{} 
+        double compute_cost(
+            std::list<std::shared_ptr<Variables<T>>> predictions,
+            std::list<std::shared_ptr<Variables<T>>> targets
+	){
+		auto predictions_iter = predictions.begin();
+		auto targets_iter = targets.begin();
+
+		for(;
+		    predictions_iter != predictions.end() &&
+		    targets_iter != targets.end();
+		    ++predictions_iter, ++targets_iter) {
+			auto prediction = predictions_iter->get_values();
+		}
+		return 0;
+	}
+    private:
+	Cost<T> single_cost;
+};
+
 #endif
